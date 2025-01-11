6059d431141ef0fe93baf212dde2d4b21f8fe4ba
jefflueck <lueckcfd@yahoo.com>
2025-01-10 20:36:33 -0600

    reverted back to last commit, added validation to email and password of form, added onSaved for form inputs.

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -10,6 +10,21 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _form = GlobalKey<FormState>();
+
+  var _isLogin = true;
+  var _enteredEmail = '';
+  var _enteredPassword = '';
+
+  void _submit() {
+    final isValid = _form.currentState!.validate();
+    if (isValid) {
+      _form.currentState!.save();
+      print(_enteredEmail);
+      print(_enteredPassword);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,6 +50,7 @@ class _AuthScreenState extends State<AuthScreen> {
                     child: Padding(
                       padding: const EdgeInsets.all(16),
                       child: Form(
+                        key: _form,
                         child: Column(
                           mainAxisSize: MainAxisSize.min,
                           children: [
@@ -45,12 +61,53 @@ class _AuthScreenState extends State<AuthScreen> {
                               keyboardType: TextInputType.emailAddress,
                               autocorrect: false,
                               textCapitalization: TextCapitalization.none,
+                              validator: (value) {
+                                if (value == null ||
+                                    value.trim().isEmpty ||
+                                    !value.contains('@')) {
+                                  return 'Please enter a valid email address.';
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                _enteredEmail = value!;
+                              },
                             ),
                             TextFormField(
                               decoration: const InputDecoration(
                                 labelText: 'Password',
                               ),
                               obscureText: true,
+                              validator: (value) {
+                                if (value == null ||
+                                    value.length < 6 ||
+                                    value.isEmpty) {
+                                  return 'Password must be at least 6 characters long and cannot be empty.';
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                _enteredPassword = value!;
+                              },
+                            ),
+                            const SizedBox(height: 12),
+                            ElevatedButton(
+                              onPressed: _submit,
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .primaryContainer,
+                              ),
+                              child: Text(_isLogin ? 'Login' : 'Signup'),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isLogin = !_isLogin;
+                                });
+                              },
+                              child: Text(
+                                  _isLogin ? 'Create new account' : 'Login'),
                             ),
                           ],
                         ),
